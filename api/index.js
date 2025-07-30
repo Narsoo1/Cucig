d3401ba72681bed6c03dbc7c8a8ac5be2b47ef06
vlamodix <duriahrealmenarzo@gmail.com>
2025-07-30 08:53:48 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,11 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import serverless from 'serverless-http';
 
-import checkUsername from '../controllers/checkUsername.js';
-import deleteChatHistory from '../controllers/deleteChatHistory.js';
-import logNewUser from '../controllers/logNewUser.js';
-import sendChatNotification from '../controllers/sendChatNotification.js';
-import updateReadStatus from '../controllers/updateReadStatus.js';
+import checkUsername from './controllers/checkUsername.js';
+import deleteChatHistory from './controllers/deleteChatHistory.js';
+import logNewUser from './controllers/logNewUser.js';
+import sendChatNotification from './controllers/sendChatNotification.js';
+import updateReadStatus from './controllers/updateReadStatus.js';
 
 const app = express();
 app.use(cors());
