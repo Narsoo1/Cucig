bac9fa205111d8e8f3731d2d55785a7a4fceaba6
vlamodix <duriahrealmenarzo@gmail.com>
2025-07-30 08:06:21 +0700

    Create index.js

---
+++ controllers/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import serverless from 'serverless-http';
+
+import checkUsername from '../controllers/checkUsername.js';
+import deleteChatHistory from '../controllers/deleteChatHistory.js';
+import logNewUser from '../controllers/logNewUser.js';
+import sendChatNotification from '../controllers/sendChatNotification.js';
+import updateReadStatus from '../controllers/updateReadStatus.js';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post('/checkUsername', checkUsername);
+app.post('/deleteChatHistory', deleteChatHistory);
+app.post('/logNewUser', logNewUser);
+app.post('/sendChatNotification', sendChatNotification);
+app.post('/updateReadStatus', updateReadStatus);
+
+export const handler = serverless(app);
